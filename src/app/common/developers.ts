683a505c9a15485e84b4dcdb61009c8f4a2f1d0f
Micah Torres <micah.torres@aa.com>
2018-05-28 17:10:09 -0700

    Auto-generated commit

--- src/app/common/developers.ts
+++
@@ -1,8 +0,0 @@
-import { Projects } from './projects';
-
-export interface Developers {
-  name: string;
-  repo: string;
-  count: number;
-  projects: Projects[];
-}
--- src/app/common/projects.ts
+++
@@ -1,4 +0,0 @@
-export interface Projects {
-  name: string;
-  url: string;
-}
